567a26486f559793c4403f3ab43f725420f5b1ad
senagab <senagab@gmail.com>
2023-11-16 17:31:35 -0400

    feat: alterações em nomes de pastas, mascara do form, scripts e pasta de script com validação e mascara min, alt nas imagens, tradução dos nomes das variáveis

---
+++ js/main.js
@@ -0,0 +1,6 @@
+
+const typeWriter = document.getElementById('typewriter-text');
+const text = 'Enlightening Businesses, Generating Growth.';
+
+typeWriter.innerHTML = text;
+typeWriter.style.setProperty('--characters', text.length)
