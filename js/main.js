ce5a182df125c8f8f87b7ff8ac79b344174754ac
senagab <senagab@gmail.com>
2023-11-16 20:08:00 -0400

    feat: validação form e mascara

--- js/main.js
+++ js/main.js
@@ -1,6 +1,38 @@
 
-const typeWriter = document.getElementById('typewriter-text');
-const text = 'Enlightening Businesses, Generating Growth.';
+$(document).ready(function() {
+    
+    $('#telefone').mask('(00) 00000-0000')
 
-typeWriter.innerHTML = text;
-typeWriter.style.setProperty('--characters', text.length)
+    $('form').validate({
+        rules: { /* regras se aplicam aos inputs com propriedade nome*/
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            mensagem: {
+                required: true,
+            }
+        },
+        messages: { /* edição da mensagem para nome */
+            nome: 'Por favor insira seu nome'   
+        },
+        submitHandler: function(form) {
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`existem ${camposIncorretos} campos incorretos`)
+            }
+        }
+    })
+    
+    const typeWriter = document.getElementById('typewriter-text');
+    const text = 'Enlightening Businesses, Generating Growth.';
+
+    typeWriter.innerHTML = text;
+    typeWriter.style.setProperty('--characters', text.length)
+})
